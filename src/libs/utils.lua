6a059ca0397831c1a71e94047660a41358958dd0
Jonathan Toppins <jtoppins@users.noreply.github.com>
2020-02-01 23:49:39 -0500

    readlua: read in a lua file
    Read in a generic LUA file and return the table identified by the name.

--- src/libs/utils.lua
+++ src/libs/utils.lua
@@ -3,9 +3,9 @@
 
 local utils = {}
 
-function utils.foreach(ctx, itr, fcn, array, arg)
+function utils.foreach(ctx, itr, fcn, array, ...)
 	for k, v in itr(array) do
-		fcn(ctx, k, v, arg)
+		fcn(ctx, k, v, unpack({select(1, ...)}))
 	end
 end
 
@@ -45,6 +45,18 @@ function utils.mergetables(dest, source)
 		dest[k] = v
 	end
 	return dest
+end
+
+function utils.readlua(file, tblname)
+	assert(file and type(file) == "string", "file path must be provided")
+	assert(tblname and type(tblname) == "string", "tblname must be provided")
+	assert(lfs.attributes(file) ~= nil, "file does not exist: "..file)
+	assert(pcall(dofile, file), "failed to parse: "..file)
+	assert(_G[tblname] ~= nil, string.format("parsing of '%s' didn't "..
+		"contain expected symbol '%s'", file, tblname))
+	local data = _G[tblname]
+	_G[tblname] = nil
+	return data
 end
 
 -- return the directory seperator used for the given OS
