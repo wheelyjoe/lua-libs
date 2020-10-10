e2b1670bf3efce9a977abdc0fd995673821e1f9d
Jonathan Toppins <jtoppins@users.noreply.github.com>
2020-10-09 21:17:35 -0400

    utils: add checkkeys and getkey functions

--- src/libs/utils.lua
+++ src/libs/utils.lua
@@ -3,6 +3,15 @@
 
 local utils = {}
 
+function utils.getkey(tbl, val)
+	for k, v in pairs(tbl) do
+		if v == val then
+			return k
+		end
+	end
+	return nil
+end
+
 function utils.foreach(ctx, itr, fcn, array, ...)
 	for k, v in itr(array) do
 		fcn(ctx, k, v, unpack({select(1, ...)}))
@@ -66,6 +75,31 @@ function utils.readconfigs(cfgfiles, tbl)
 			utils.mergetables(tbl[cfg.name],
 				cfg.validate(cfg,
 					utils.readlua(cfg.file, cfg.cfgtblname)))
+		end
+	end
+end
+
+local function errorhandler(key, m, path)
+	local msg = string.format("%s: %s; file: %s",
+		key, m, path or "nil")
+	error(msg, 2)
+end
+
+function utils.checkkeys(keys, tbl)
+	for _, keydata in ipairs(keys) do
+		if keydata.default == nil and tbl[keydata.name] == nil then
+			errorhandler(keydata.name, "missing required key", tbl.path)
+		elseif keydata.default ~= nil and tbl[keydata.name] == nil then
+			tbl[keydata.name] = keydata.default
+		else
+			if type(tbl[keydata.name]) ~= keydata.type then
+				errorhandler(keydata.name, "invalid key value", tbl.path)
+			end
+
+			if type(keydata.check) == "function" and
+				not keydata.check(keydata, tbl) then
+				errorhandler(keydata.name, "invalid key value", tbl.path)
+			end
 		end
 	end
 end
